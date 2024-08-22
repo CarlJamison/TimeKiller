68f27561dd6ecfe87e491fef54f90b23f5649fb8
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2024-08-22 09:21:39 -0500

    Update ether.js

--- ether.js
+++ ether.js
@@ -1,4 +1,4 @@
-var scale = 2;
+var scale = 1;
 var sheet = [];	
 var pallet = [
 	"#f2f6f0",
@@ -8,7 +8,6 @@ var pallet = [
 	"#929383",
 ]
 
-var lastSheet = [];
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var width = Math.floor(window.innerWidth / scale) - 3;
@@ -17,20 +16,19 @@ canvas.width = width * scale;
 canvas.height = height * scale;
 
 for(var i = 0; i < height; i++){
-	lastSheet[i] = [];
-
 	sheet[i] = [];
 	for(var j = 0; j < width; j++){
 		sheet[i][j] = Math.floor(Math.random() * pallet.length)
 	}
 }
 
-window.setInterval(runFrame, 1);
-refreshCanvas();
-
 var counter = 0;
+var lastVal = null;
+
+runFrame();
+
 function runFrame(){
-	for(var i = 0; i < 30000; i++){
+	for(var i = 0; i < 100000; i++){
 		var row = Math.floor(Math.random() * (height - 2)) + 1;
 		var col = Math.floor(Math.random() * (width - 2)) + 1;
 		
@@ -38,34 +36,20 @@ function runFrame(){
 		var d = [{r: -1, c: 0}, {r: -1, c: 1}, {r: 0, c: 1}, {r: 1, c: 1}, 
 			{r: 1, c: 0}, {r: 1, c: -1}, {r: 0, c: -1}, {r: -1, c: -1}][counter]
 
+		var value = sheet[row + d.r][col + d.c];
 
-		sheet[row][col] = sheet[row + d.r][col + d.c];
-	}
-}
-
-function refreshCanvas(){
-	var lastVal = null;
-	
-	for(var i = 0; i < height; i++){
-		var row = sheet[i];
-		var lastRow = lastSheet[i];
-
-		for(var j = 0; j < width; j++){
-			
-			var value = row[j];
-			
-			if(value != lastRow[j]){
-				if(value != lastVal){
-					ctx.fillStyle = pallet[value];
-					lastVal = value;
-				}
-			
-				ctx.fillRect(j * scale, i * scale, scale, scale);
-				lastRow[j] = value;
-			}	
+		if(sheet[row][col] != value){
+			sheet[row][col] = value;
+		
+			if(value != lastVal){
+				ctx.fillStyle = pallet[value];
+				lastVal = value;
+			}
+		
+			ctx.fillRect(col * scale, row * scale, scale, scale);
 		}
+		
 	}
-	
-	requestAnimationFrame(refreshCanvas);
+
+	requestAnimationFrame(runFrame);
 }
-  
