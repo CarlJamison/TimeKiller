9e1c6c0e0e35af113901a9a1a14a5eb6d59dd85d
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2024-09-09 17:19:24 -0500

    Update cga2A.js

--- Misc/cga2A.js
+++ Misc/cga2A.js
@@ -1,52 +1,27 @@
-const scale = 2;
 var instruction = [];
 var cost = [];
 var paths = [];
 var max = 0.0;
 var paths = [];
-var mode = "COST";
+var mode = "q";
 const DIRECTIONS = [{r: -1, c: 0}, {r: -1, c: 1}, {r: 0, c: 1}, {r: 1, c: 1}, 
 	{r: 1, c: 0}, {r: 1, c: -1}, {r: 0, c: -1}, {r: -1, c: -1}];
 
-const width = Math.floor(window.innerWidth / scale);
-const height = Math.floor(window.innerHeight / scale);
-
 const canvas = document.getElementById("myCanvas");
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
-canvas.width = width * scale;
-canvas.height = height * scale;
-var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+const imgData = ctx.getImageData(0, 0, width, height);
+const buf = new Uint32Array(imgData.data.buffer);
 
-const costField = Array.from(Array(height), () => Array.from(Array(width), Math.random));
+const costField = Array.from(Array(height * width), Math.random);
 
 addEventListener("mousedown", onClick);
 addEventListener("mousemove", draw);
 addEventListener("keydown", event => {
-	switch(event.key) {
-		case "q":
-			if(mode != "COST"){
-				mode = "COST";
-				generateImage();
-			}
-			break;
-		case "w":
-			if(mode != "TOTAL_COST"){
-				mode = "TOTAL_COST";
-				generateImage();
-			}
-			break;
-		case "e":
-			if(mode != "PATHS"){
-				mode = "PATHS";
-				generateImage();
-			}
-			break;
-		case "r":
-			if(mode != "COMBINED"){
-				mode = "COMBINED";
-				generateImage();
-			}
-			break;
+	if("qwer".indexOf(event.key) > -1 && mode != event.key){
+		mode = event.key;
+		generateImage()
 	}
 });
 
@@ -54,82 +29,50 @@ render(Math.floor(height / 2), Math.floor(width / 2));
 
 function generateImage(){
 
-	if(mode == "COST"){
-		for(var i = 0; i < width; i++){
-			for(var j = 0; j < height; j++){
-				
-				var value = 256 * costField[j][i];
-
-				ctx.fillStyle = `rgb(${value},${value},${value}`;
-				ctx.fillRect(i * scale, j * scale, scale, scale);
-			}
+	if(mode == "q"){
+		for(let i = 0; i < height * width; i++){
+			var value = Math.floor(256 * costField[i]).toString(16).padStart(2, '0');;
+			buf[i] = `0xFF${value}${value}${value}`;
 		}
-	}else if(mode == "TOTAL_COST"){
-		for(var i = 0; i < width; i++){
-			for(var j = 0; j < height; j++){
-				
-				var value = 256 - (cost[i + (j * width)] / max * 256)
-			
-				ctx.fillStyle = `rgb(${value},${value},${value}`;
-				ctx.fillRect(i * scale, j * scale, scale, scale);
-			}
+	}else if(mode == "w"){
+		for(let i = 0; i < height * width; i++){
+			const value = Math.floor(256 - (cost[i] / max * 256)).toString(16).padStart(2, '0');
+			buf[i] = `0xFF${value}${value}${value}`;
 		}
-	}else if(mode == "PATHS"){
-		var hitList = [];
-		for(var i = 0; i < width * height; i++){
-			hitList[i] = 0
-		}
-		for(var i = 0; i < width * height; i++){
-			var nextInstruction = instruction[i];
-			while(instruction[nextInstruction]){
-				hitList[nextInstruction]++;
-				nextInstruction = instruction[nextInstruction];
-			}
+	}else if(mode == "e"){
+		let hitList = Array(width * height).fill(0);
+		for(let i = 0; i < width * height; i++){
+			for(let nextInstruction = i; nextInstruction = instruction[nextInstruction]; hitList[nextInstruction]++){}
 		}
 
-		var maxInst = 0;
-		hitList.forEach(h => maxInst = h > maxInst ? h : maxInst);
-		for(var i = 0; i < width; i++){
-			for(var j = 0; j < height; j++){
-				var value = 256 * Math.pow(hitList[i + (j * width)] / maxInst, .25);
-				ctx.fillStyle = `rgb(${value},${value},${value}`;
-				ctx.fillRect(i * scale, j * scale, scale, scale);
-			}
+		let maxInst = hitList.reduce((max, h) => max = h > max ? h : max, 0);
+		for(let i = 0; i < height * width; i++){
+			const value = Math.floor(256 * Math.pow(hitList[i] / maxInst, .2))
+				.toString(16).padStart(2, '0');
+			buf[i] = `0xFF${value}${value}${value}`;
 		}
-	}else if(mode == "COMBINED"){
-		var hitList = [];
-		for(var i = 0; i < width * height; i++){
-			hitList[i] = 0
-		}
-		for(var i = 0; i < width * height; i++){
-			var nextInstruction = instruction[i];
-			while(instruction[nextInstruction]){
-				hitList[nextInstruction]++;
-				nextInstruction = instruction[nextInstruction];
-			}
+	}else if(mode == "r"){
+		let hitList = Array(width * height).fill(0);
+		for(let i = 0; i < width * height; i++){
+			for(let nextInstruction = i; nextInstruction = instruction[nextInstruction]; hitList[nextInstruction]++){}
 		}
 
-		var maxInst = 0;
-		hitList.forEach(h => maxInst = h > maxInst ? h : maxInst);
-		for(var i = 0; i < width; i++){
-			for(var j = 0; j < height; j++){
-				var value = 256 * Math.sqrt(hitList[i + (j * width)] / maxInst) + (256 - (cost[i + (j * width)] / max * 256))
-				value = value > 255 ? 255 : value;
-				ctx.fillStyle = `rgb(${value},${value},${value}`;
-				ctx.fillRect(i * scale, j * scale, scale, scale);
-			}
+		let maxInst = hitList.reduce((max, h) => max = h > max ? h : max, 0);
+		for(let i = 0; i < height * width; i++){
+			const raw = 256 * Math.sqrt(hitList[i] / maxInst) + (256 - (cost[i] / max * 256))
+			const value = Math.floor(Math.min(255, raw)).toString(16).padStart(2, '0');
+			buf[i] = `0xFF${value}${value}${value}`;
 		}
 	}
 
-	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
+	ctx.putImageData(imgData,0,0);
 }
 
 function draw(event){
-	ctx.putImageData(imgData, 0, 0);
+	ctx.putImageData(imgData,0,0);
     var rect = canvas.getBoundingClientRect();
-	var r = Math.floor((event.clientY - rect.top) / scale);
-	var c = Math.floor(event.clientX / scale);
+	var r = Math.floor(event.clientY - rect.top);
+	var c = Math.floor(event.clientX);
 
 	ctx.fillStyle = "rgb(255, 255, 255)";
 
@@ -138,30 +81,24 @@ function draw(event){
 		var col = instCoord % width;
 		var row = Math.floor(instCoord / width);
 
-		ctx.fillRect(col * scale, row * scale, scale, scale);
+		ctx.fillRect(col, row, 1, 1);
 		instCoord = instruction[instCoord];
 	}
 }
 
 function onClick(event){
-	var canvas = document.getElementById("myCanvas");
     var rect = canvas.getBoundingClientRect();
     var y = event.clientY - rect.top;
     var x = event.clientX;
-	
-	var iStart = Math.floor(y / scale);
-	var jStart = Math.floor(x / scale);
-	render(iStart, jStart);
+	render(y, x);
 }
 
 function render(iStart, jStart){
 	let start = Date.now();
-	const r = height;
-	const c = width;
 	
-	ready = [{ id: iStart * c + jStart, cost: 0 }];
+	ready = [{ id: iStart * width + jStart, cost: 0 }];
 	cost = [];
-	cost[iStart * c + jStart] = 0;
+	cost[iStart * width + jStart] = 0;
 	instruction = [];
 	
 	while(ready.length){
@@ -171,18 +108,20 @@ function render(iStart, jStart){
 		ready[min.index] = ready[ready.length - 1];
 		ready.pop();
 		
-		const cR = Math.floor(min.id / c);
-		const cC = min.id % c;
+		const cR = Math.floor(min.id / width);
+		const cC = min.id % width;
 
 		DIRECTIONS.forEach(d => {
-			const norm = (Math.abs(d.r) + Math.abs(d.c)) == 1 ? 1 : Math.sqrt(2);
-			if(cR + d.r > -1 && cR + d.r < r && cC + d.c > -1 && cC + d.c < c){
-				addInstruction((cR + d.r) * c + cC + d.c, min.cost + (norm * costField[cR + d.r][cC + d.c]), min.id);
+			if(cR + d.r > -1 && cR + d.r < height && cC + d.c > -1 && cC + d.c < width){
+				let index = (cR + d.r) * width + cC + d.c;
+				let norm = (Math.abs(d.r) + Math.abs(d.c)) == 1 ? 1 : Math.sqrt(2);
+				addInstruction(index, min.cost + (norm * costField[index]), min.id);
 			}
 		});
 		
 		max = min.cost;
 	}
+
 	generateImage();
 	console.log(Date.now() - start);
 }
