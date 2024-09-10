bcad2d6d891a53139f4a6068acd70c0c50a556d8
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2024-09-10 09:01:14 -0500

    clean up lightning

--- Misc/cga2A.js
+++ Misc/cga2A.js
@@ -11,6 +11,7 @@ const canvas = document.getElementById("myCanvas");
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
+ctx.fillStyle = "rgb(255, 255, 255)";
 const imgData = ctx.getImageData(0, 0, width, height);
 const buf = new Uint32Array(imgData.data.buffer);
 
@@ -25,64 +26,56 @@ addEventListener("keydown", event => {
 	}
 });
 
-render(Math.floor(height / 2), Math.floor(width / 2));
+renderMap(0, 0);
 
 function generateImage(){
 
 	if(mode == "q"){
 		for(let i = 0; i < height * width; i++){
-			var value = Math.floor(256 * costField[i]).toString(16).padStart(2, '0');;
-			buf[i] = `0xFF${value}${value}${value}`;
+			render(i, costField[i]);
 		}
 	}else if(mode == "w"){
 		for(let i = 0; i < height * width; i++){
-			const value = Math.floor(256 - (cost[i] / max * 256)).toString(16).padStart(2, '0');
-			buf[i] = `0xFF${value}${value}${value}`;
+			render(i, 1 - (cost[i] / max))
 		}
-	}else if(mode == "e"){
+	}else if(mode == "e" || mode == "r"){
 		let hitList = Array(width * height).fill(0);
 		for(let i = 0; i < width * height; i++){
-			for(let nextInstruction = i; nextInstruction = instruction[nextInstruction]; hitList[nextInstruction]++){}
+			for(let nextInst = i; nextInst = instruction[nextInst]; hitList[nextInst]++){}
 		}
 
 		let maxInst = hitList.reduce((max, h) => max = h > max ? h : max, 0);
-		for(let i = 0; i < height * width; i++){
-			const value = Math.floor(256 * Math.pow(hitList[i] / maxInst, .2))
-				.toString(16).padStart(2, '0');
-			buf[i] = `0xFF${value}${value}${value}`;
-		}
-	}else if(mode == "r"){
-		let hitList = Array(width * height).fill(0);
-		for(let i = 0; i < width * height; i++){
-			for(let nextInstruction = i; nextInstruction = instruction[nextInstruction]; hitList[nextInstruction]++){}
-		}
 
-		let maxInst = hitList.reduce((max, h) => max = h > max ? h : max, 0);
-		for(let i = 0; i < height * width; i++){
-			const raw = 256 * Math.sqrt(hitList[i] / maxInst) + (256 - (cost[i] / max * 256))
-			const value = Math.floor(Math.min(255, raw)).toString(16).padStart(2, '0');
-			buf[i] = `0xFF${value}${value}${value}`;
+		if(mode == "e"){
+			for(let i = 0; i < height * width; i++){
+				render(i, (hitList[i] / maxInst) ** .2);
+			}
+		}else{
+			for(let i = 0; i < height * width; i++){
+				render(i, Math.min(1, ((hitList[i] / maxInst) ** .5) + (1 - (cost[i] / max))));
+			}
 		}
 	}
 
 	ctx.putImageData(imgData,0,0);
 }
 
+function render(i, raw){
+	let value = Math.floor(256 * raw).toString(16).padStart(2, '0')
+	buf[i] = `0xFF${value}${value}${value}`;
+}
+
 function draw(event){
 	ctx.putImageData(imgData,0,0);
     var rect = canvas.getBoundingClientRect();
-	var r = Math.floor(event.clientY - rect.top);
-	var c = Math.floor(event.clientX);
-
-	ctx.fillStyle = "rgb(255, 255, 255)";
+	var r = event.clientY - rect.top;
+	var c = event.clientX;
 
 	var instCoord = (r * width) + c;
-	while(instruction[instCoord]){
+	while(instCoord = instruction[instCoord]){
 		var col = instCoord % width;
 		var row = Math.floor(instCoord / width);
-
 		ctx.fillRect(col, row, 1, 1);
-		instCoord = instruction[instCoord];
 	}
 }
 
@@ -90,10 +83,10 @@ function onClick(event){
     var rect = canvas.getBoundingClientRect();
     var y = event.clientY - rect.top;
     var x = event.clientX;
-	render(y, x);
+	renderMap(y, x);
 }
 
-function render(iStart, jStart){
+function renderMap(iStart, jStart){
 	let start = Date.now();
 	
 	ready = [{ id: iStart * width + jStart, cost: 0 }];
@@ -108,12 +101,12 @@ function render(iStart, jStart){
 		ready[min.index] = ready[ready.length - 1];
 		ready.pop();
 		
-		const cR = Math.floor(min.id / width);
-		const cC = min.id % width;
+		const r = Math.floor(min.id / width);
+		const c = min.id % width;
 
 		DIRECTIONS.forEach(d => {
-			if(cR + d.r > -1 && cR + d.r < height && cC + d.c > -1 && cC + d.c < width){
-				let index = (cR + d.r) * width + cC + d.c;
+			if(r + d.r > -1 && r + d.r < height && c + d.c > -1 && c + d.c < width){
+				let index = (r + d.r) * width + c + d.c;
 				let norm = (Math.abs(d.r) + Math.abs(d.c)) == 1 ? 1 : Math.sqrt(2);
 				addInstruction(index, min.cost + (norm * costField[index]), min.id);
 			}
