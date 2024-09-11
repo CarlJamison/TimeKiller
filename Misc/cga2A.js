8d1c5a5bd525b4372ebb4e9f6fd8aa5fa5cde3a4
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2024-09-11 12:21:34 -0500

    Update cga2A.js

--- Misc/cga2A.js
+++ Misc/cga2A.js
@@ -1,11 +1,10 @@
 var instruction = [];
 var cost = [];
-var paths = [];
-var max = 0.0;
-var paths = [];
-var mode = "w";
+var hitList = [];
+var mode = "r";
 const DIRECTIONS = [{r: -1, c: 0}, {r: -1, c: 1}, {r: 0, c: 1}, {r: 1, c: 1}, 
-	{r: 1, c: 0}, {r: 1, c: -1}, {r: 0, c: -1}, {r: -1, c: -1}];
+	{r: 1, c: 0}, {r: 1, c: -1}, {r: 0, c: -1}, {r: -1, c: -1}]
+	.map(d => ({...d, norm: (Math.abs(d.r) + Math.abs(d.c)) == 1 ? 1 : Math.sqrt(2)}));
 
 const canvas = document.getElementById("myCanvas");
 const width = canvas.width = window.innerWidth;
@@ -21,33 +20,23 @@ const costField = Array.from(Array(height * width), Math.random);
 addEventListener("mousedown", e => renderMap(e.clientY - boundTop, e.clientX));
 addEventListener("mousemove", draw);
 addEventListener("keydown", event => {
-	if("qwer".indexOf(event.key) > -1 && mode != event.key){
-		mode = event.key;
-		generateImage();
-	}
+	if("qwer".indexOf(event.key) > -1) mode = event.key;
+	generateImage();
 });
 
 renderMap(0, 0);
 
 function generateImage(){
 
+	let max = cost.reduce((m, h) => h > m ? h : m, 0);
 	if(mode == "q"){
 		costField.forEach((v, i) => render(i, v));
 	}else if(mode == "w"){
 		cost.forEach((v, i) => render(i, 1 - (v / max)))
-	}else if(mode == "e" || mode == "r"){
-		let hitList = Array(width * height).fill(0);
-		for(let i = 0; i < width * height; i++){
-			for(let nextInst = i; nextInst = instruction[nextInst]; hitList[nextInst]++){}
-		}
-
-		let maxInst = hitList.reduce((max, h) => max = h > max ? h : max, 0);
-
-		if(mode == "e"){
-			hitList.forEach((v, i) => render(i, (v / maxInst) ** .2))
-		}else{
-			hitList.forEach((v, i) => render(i, Math.min(1, (v / maxInst) ** .5 + 1 - cost[i] / max)));
-		}
+	}else if(mode == "e"){
+		hitList.forEach((v, i) => render(i, (v / (hitList.length - 1)) ** .2))
+	}else if(mode == "r"){
+		hitList.forEach((v, i) => render(i, Math.min(1, (v / (hitList.length - 1)) ** .5 + 1 - cost[i] / max)));
 	}
 
 	ctx.putImageData(imgData,0,0);
@@ -61,10 +50,10 @@ function render(i, raw){
 function draw(event){
 	ctx.putImageData(imgData,0,0);
 
-	var instCoord = ((event.clientY - boundTop) * width) + event.clientX;
+	let instCoord = ((event.clientY - boundTop) * width) + event.clientX;
 	while(instCoord = instruction[instCoord]){
-		var col = instCoord % width;
-		var row = Math.floor(instCoord / width);
+		let col = instCoord % width;
+		let row = Math.floor(instCoord / width);
 		ctx.fillRect(col, row, 1, 1);
 	}
 }
@@ -72,46 +61,41 @@ function draw(event){
 function renderMap(iStart, jStart){
 	let start = Date.now();
 	
-	ready = [{ id: iStart * width + jStart, cost: 0 }];
+	const ready = [{ id: iStart * width + jStart, cost: 0 }];
 	cost = [];
 	cost[iStart * width + jStart] = 0;
 	instruction = [];
+	hitList = Array(width * height).fill(0);
 	
 	while(ready.length){
-		const min = ready.reduce(
-			(min, x, index) => x.cost < min.cost ? {index, cost: x.cost, id: x.id} : min, { cost: Infinity });
+		const min = ready.reduce((min, x) => x.cost < min.cost ? x : min, { cost: Infinity });
 
-		ready[min.index] = ready[ready.length - 1];
+		ready[ready.indexOf(min)] = ready[ready.length - 1];
 		ready.pop();
-		
+
+		for(let nextInst = min.id; nextInst = instruction[nextInst]; hitList[nextInst]++){}
+
 		const r = Math.floor(min.id / width);
 		const c = min.id % width;
 
 		DIRECTIONS.forEach(d => {
 			if(r + d.r > -1 && r + d.r < height && c + d.c > -1 && c + d.c < width){
 				let index = (r + d.r) * width + c + d.c;
-				let norm = (Math.abs(d.r) + Math.abs(d.c)) == 1 ? 1 : Math.sqrt(2);
-				addInstruction(index, min.cost + (norm * costField[index]), min.id);
+				let newCost = min.cost + (d.norm * costField[index]);
+
+				if(cost[index] == null){
+					ready.push({ id: index, cost: newCost });
+					instruction[index] = min.id;
+					cost[index] = newCost;
+				}else if(cost[index] > newCost){
+					ready.find(x => x.id == index).cost = newCost;
+					instruction[index] = min.id;
+					cost[index] = newCost;
+				}
 			}
 		});
-		
-		max = min.cost;
 	}
 
 	generateImage();
 	console.log(Date.now() - start);
-}
-	
-function addInstruction(newVal, newCost, min) {
-	var existingCost = cost[newVal];
-
-	if(existingCost == null){
-		ready.push({ id: newVal, cost: newCost });
-		instruction[newVal] = min;
-		cost[newVal] = newCost;
-	}else if(existingCost > newCost){
-		ready.find(x => x.id == newVal).cost = newCost;
-		instruction[newVal] = min;
-		cost[newVal] = newCost;
-	}
 }
